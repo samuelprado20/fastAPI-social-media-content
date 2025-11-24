7d0af7d52d86f6070c009499bf4884141792ebef
Samuel <samuel.prado.osorio20@gmail.com>
2025-11-24 00:58:16 -0500

    Add posts POST endpoint

--- app/app.py
+++ app/app.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, HTTPException
+from app.schemas import PostCreate, PostResponse
+
 
 app = FastAPI()
 
@@ -20,11 +22,20 @@ text_posts = {
 }
 
 @app.get("/posts")
-def get_all_posts():
+def get_all_posts(limit: int = None):
+  if limit:
+    return list(text_posts.values())[:limit]
   return text_posts
 
 @app.get("/posts/{id}")
-def get_post(id: int):
+def get_post(id: int) -> PostResponse:
   if id not in text_posts:
     raise HTTPException(status_code=404, detail="Post not found")
   return text_posts.get(id)
+
+@app.post("/posts")
+def create_post(post: PostCreate) -> PostResponse:
+  new_post = {"title": post.title, "content": post.content}
+  text_posts[max(text_posts.keys()) + 1] = new_post
+
+  return new_post
